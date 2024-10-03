259ea2c82b0c1f931565079776da60fb9db0a7d6
Dennis Enderink <dennis@1815.nl>
2024-10-03 11:31:14 +0200

    fix: fix namespace of class Hook

--- src/Hook/Hook.php
+++ src/Hook/Hook.php
@@ -1,13 +1,27 @@
 <?php
 
-namespace Achttienvijftien\Stud\Hook;
+namespace AchttienVijftien\Stud\Hook;
 
 use AchttienVijftien\Stud\BootableInterface;
 
+/**
+ * Class Hook.
+ *
+ * @package AchttienVijftien\Stud\Hook
+ */
 class Hook implements BootableInterface {
+
+	/**
+	 * Hook constructor.
+	 *
+	 * @param iterable $services The tagged services.
+	 */
 	public function __construct( private iterable $services ) {
 	}
 
+	/**
+	 * Calls `add_hooks` method on all tagged services on boot.
+	 */
 	public function boot(): void {
 		foreach ( $this->services as $service ) {
 			$service->add_hooks();
